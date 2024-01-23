7f62e22723ac3fa5ec97b60604c4762a92c888d9
afeiyoo <afeiyoo@gmail.com>
2024-01-23 20:12:38 +0800

    [FEAT] Show data of json with view and model
    Add:
      + Load data from model and show it with view
      + Use singleton design pattern

--- src/fend/uimain/objectswidget.cpp
+++ src/fend/uimain/objectswidget.cpp
@@ -1,11 +1,13 @@
-#include "objectswidget.h"
+﻿#include "objectswidget.h"
 #include "ui_objectswidget.h"
+#include "src/bend/man/manbuckets.h"
 
 ObjectsWidget::ObjectsWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ObjectsWidget)
 {
     ui->setupUi(this);
+    ui->tableView->setModel(MB->model());
 }
 
 ObjectsWidget::~ObjectsWidget()
